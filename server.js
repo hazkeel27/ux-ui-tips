d222a2f38cec07172aa84e80fcfa7d3e5edc2628
hazkeel <hazkeel27@gmail.com>
2023-08-16 21:09:18 -0400

    port issue fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
